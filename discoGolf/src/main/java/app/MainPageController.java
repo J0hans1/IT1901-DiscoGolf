10466ca3b6eb526c1516b34a902ac264d1079e23
BillySBarrett <billysb@stud.ntnu.no>
2022-09-18 14:15:47 +0200

    connected MainPageController to the fxml file and tested add course

--- discoGolf/src/main/java/app/MainPageController.java
+++ discoGolf/src/main/java/app/MainPageController.java
@@ -6,11 +6,19 @@ import java.util.function.UnaryOperator;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.Labeled;
 import javafx.scene.control.ListView;
 
-public class CalcController {
+public class MainPageController {
 
+
+    @FXML
+    ComboBox<String> pickCourseMenu;
+
+    public void initialize() {
+        pickCourseMenu.getItems().addAll("Lade", "Dragvoll");
+    }
 
 }
