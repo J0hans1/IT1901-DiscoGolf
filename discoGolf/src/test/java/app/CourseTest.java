acd11c54b3255965e620469ea0db814515e86ed4
Jakob Opland <jakobop@stud.ntnu.no>
2022-09-25 12:17:49 +0200

    Add more comments to tests

--- discoGolf/src/test/java/app/CourseTest.java
+++ discoGolf/src/test/java/app/CourseTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import java.util.ArrayList;
 import java.util.Arrays;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -20,27 +21,34 @@ public class CourseTest {
     }
     
     @Test
+    @DisplayName("Test the controller when creating a Course object")
     public void testController() {
         assertEquals("Dragvoll", course.getCourseName());
         assertEquals(9, course.getNumberOfHoles());
+
         course = new Course("Lade", new ArrayList<>(Arrays.asList(3, 4, 5, 3)));
         assertEquals("Lade", course.getCourseName());
         assertEquals(4, course.getNumberOfHoles());
     }
 
     @Test
+    @DisplayName("Test setParForHole method and test for invalid input")
     public void testSetParForHole() {
-        assertEquals(3, course.getParForHole(1));
-        assertEquals(5, course.getParForHole(9));
+        assertEquals(5, course.getParForHole(9),
+        "Hole 9 should have par of 5");
         course.setParForHole(9, 3);
         assertEquals(3, course.getParForHole(9),
-        "The par on hole 9 is now set to 3");
+        "The par on hole 9 should now be 3");
+        
         assertThrows(IllegalArgumentException.class, () -> {
             course.setParForHole(-1, 3);
-        }, "Not a valid hole number");
+        }, "-1 id not a valid hole number");
         assertThrows(IllegalArgumentException.class, () -> {
-            course.setParForHole(2, 2);
-        }, "Not a valid par number");
+            course.setParForHole(2, 1);
+        }, "1 is not a valid par number");
+        assertThrows(IllegalArgumentException.class, () -> {
+            course.setParForHole(2, 8);
+        }, "8 is not a valid par number");
     }
 
 }
