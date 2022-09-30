37b2b5e29605e1b2d720658b267ca65d654ff6e8
Markus Aleksander Råkil Johansen <markusrj@stud.ntnu.no>
2022-09-30 12:24:43 +0200

    Implemented deserializer and serializer interface
    currently have problems with mvn javafx:run command, but there is no dependency errors anymore
    
    Jackson dependency is finally visible
    
    Relate:#55 Authors: @markusrj @jakobop

--- discoGolf/src/main/java/app/json/parser/ScorecardSerializer.java
+++ discoGolf/src/main/java/app/json/parser/ScorecardSerializer.java
@@ -1,8 +1,10 @@
 package app.json.parser;
 
-import java.util.function.Function;
+import java.io.IOException;
 
+import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
 
 import app.Scorecard;
 
@@ -10,7 +12,8 @@ public class ScorecardSerializer extends JsonSerializer<Scorecard>{
     /**
      * Serializes a scorecard object to JSON format
      */
-    public String serialize(Scorecard scorecard){
-
+    @Override
+    public void serialize(Scorecard value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+        // TODO Auto-generated method stub
     }
 }
