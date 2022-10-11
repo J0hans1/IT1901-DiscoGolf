daaa72c694678f7be639c5ca50618e824cf44196
Markus Aleksander Råkil Johansen <markusrj@stud.ntnu.no>
2022-10-11 21:13:56 +0200

    Deserializer for Data Object
    Ready for merge
    
    Author: @jakobop @markusrj Issue: #55

--- discoGolf/core/src/main/java/discoGolf/json/parser/DataArrayDeserializer.java
+++ discoGolf/core/src/main/java/discoGolf/json/parser/DataArrayDeserializer.java
@@ -1,19 +1,34 @@
 package discoGolf.json.parser;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import com.fasterxml.jackson.core.JacksonException;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 
 import discoGolf.core.Data;
+import discoGolf.core.Scorecard;
 
 public class DataArrayDeserializer extends JsonDeserializer<Data>{
+    private ScorecardDeserializer scorecardDeserializer = new ScorecardDeserializer();
 
     @Override
     public Data deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JacksonException {
-        return null;
+        JsonNode node = p.getCodec().readTree(p);
+        ArrayList<Scorecard> list = new ArrayList<>();
+        ArrayNode jsonList = (ArrayNode) node.get("data");
+
+        for (JsonNode n : jsonList) {
+            Scorecard scorecard = scorecardDeserializer.deserialize(n);
+            list.add(scorecard);
+        }
+
+        Data data = new Data();
+        data.setData(list);
+        return data;
     }
-    
 }
