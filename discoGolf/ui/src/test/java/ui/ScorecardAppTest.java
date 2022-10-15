f154ee2d2a04deb187883d3a3f66f801ecc59626
Ulisman <uisdahl@gmail.com>
2022-10-15 17:23:52 +0200

    Add tests to ScorecardAppTest
    Testfx tests for scorecard page were previously in the DiscoGolfAppTest for convenience, but were now moved to
    their own file to make the difference between tests more clear
    Add beforeEach method to set up the internal states of the scorecardPageController
    Add testControllerTest to test that controller has been loaded
    Add findPlayerDetailsTest to test controller state values
    Add findSubmitButtonTest to test that submitbutton is visible when expected
    
    Relates: #60 Authors: @ulrikyi and @jakobop

--- discoGolf/ui/src/test/java/ui/ScorecardAppTest.java
+++ discoGolf/ui/src/test/java/ui/ScorecardAppTest.java
@@ -1,59 +1,7 @@
-// package ui;
-
-// import static org.junit.Assert.assertEquals;
-
-// import org.junit.Test;
-// import org.junit.jupiter.api.BeforeEach;
-// import org.testfx.framework.junit5.ApplicationTest;
-
-// import discoGolf.core.Course;
-// import javafx.fxml.FXMLLoader;
-// import javafx.scene.Parent;
-// import javafx.scene.Scene;
-// import javafx.stage.Stage;
-
-// public class ScorecardAppTest extends ApplicationTest{
-//      /**
-//      Method for starting the javafx application
-//      @param stage javafx Stage object
-//      */
-
-//     private ScorecardPageController controller;
-//     private Course c;
-
-//     @Override
-//     public void start(Stage stage) throws Exception {
-//         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("ScorecardTest.fxml"));
-//         Parent parent = fxmlLoader.load();
-//         stage.setScene(new Scene(parent));
-//         this.controller = fxmlLoader.getController();
-//         stage.show();
-//     }
-
-//     @BeforeEach
-//     public void setup(){
-//         this.c = new Course(null, null);
-//     }
-
-//     @Test
-//     public void testUI() throws InterruptedException{
-//         clickOn("#addThrowButton");
-//         Thread.sleep(1000);
-//         assertEquals(new String("Hei"), this.c);
-//     }
-// }
-
-
-
-
 package ui;
 
-import static org.junit.Assert.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -61,28 +9,29 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.testfx.framework.junit5.ApplicationTest;
-import org.testfx.matcher.control.ComboBoxMatchers;
-import org.testfx.matcher.control.LabeledMatchers;
 import org.testfx.util.WaitForAsyncUtils;
 
 import discoGolf.core.Course;
 import discoGolf.core.Scorecard;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.Labeled;
-import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
 
-public class ScorecardAppTest extends ApplicationTest {
-    
+public class ScorecardAppTest extends ApplicationTest{
+
     private Course lade;
-    private Course dragvoll;
+    private Scorecard scorecard;
+    @FXML
     private ScorecardPageController controller;
+    @FXML
+    public Label currentCourseLabel, displayPlayerName, currentHole, currentScore, totalScoreLabel, currentHoleParLabel;
+    @FXML
+    public Button previousHoleButton, nextHoleButton, submitBtn;
 
     /**
      Method for starting the javafx application
@@ -99,35 +48,87 @@ public class ScorecardAppTest extends ApplicationTest {
 
     @BeforeEach
     public void setUpItems(){
-        // Scorecard scorecard = new Scorecard(null, null);
-        lade = new Course("Lade",new ArrayList<>(Arrays.asList(3,4,3,4,3,4,3,4,3)));
-        dragvoll = new Course("Dragvoll", new ArrayList<>(Arrays.asList(3,4,3,4,3,4,3,4,3,4,3,4,3,4,3,4,3,4)));
+        this.lade = new Course("Lade",new ArrayList<>(Arrays.asList(3,4,3,4,3,4,3,4,3)));
+
+        this.scorecard = new Scorecard(this.lade, "Ulrik");
+        Platform.runLater(()->{ //Synchronize the test with the JavaFX thread
+            this.displayPlayerName = lookup("#displayNameOfPlayer").query();
+            this.currentCourseLabel = lookup("#currentCourseLabel").query();
+            this.displayPlayerName.setText("Name: " + scorecard.getPlayerName());    
+            this.currentCourseLabel.setText("Course: " + scorecard.getCourseName());
+
+            this.currentHoleParLabel = lookup("#currentHoleParLabel").query();
+            this.previousHoleButton = lookup("#previousHoleButton").query();
+            this.nextHoleButton = lookup("#nextHoleButton").query();
+            this.currentHole = lookup("#currentHole").query();
+            this.currentScore = lookup("#currentScore").query();
+            this.submitBtn = lookup("#submitBtn").query();
+            
+            updateInfoDisplay();
+            handleBtnVisibilty();
+        });
+    }
+    
+    @Test
+    @DisplayName("Test that the controller has been loaded")
+    public void testController(){
+        assertNotNull(this.controller);
+    }
+    
+    @Test
+    @DisplayName("Test that elements regarding scorecard values are present on the page")
+    public void findPlayerDetails(){
+        String playerName = this.scorecard.getPlayerName();
+        WaitForAsyncUtils.waitForFxEvents();
+        assertNotNull(lookup(playerName));
+        
+        String courseName = this.scorecard.getCourseName();
+        WaitForAsyncUtils.waitForFxEvents();
+        assertNotNull(lookup(courseName));
+        
+        Integer totalScore = this.scorecard.getTotalScore();
+        WaitForAsyncUtils.waitForFxEvents();
+        assertNotNull(lookup(totalScore.toString())); //Check that there is a element with the value 'totalScore' in the page
+    }
+    
+    @Test
+    @DisplayName("Test that the submitbutton is available after the end of the Lade course is reached")
+    public void findSubmitButtonLadeCourse(){
+        iterateThroughCourse(scorecard.getCourse());
+        Button submitBtn = lookup("Submit").query();
+        WaitForAsyncUtils.waitForFxEvents();
+        assertNotNull(submitBtn);
+    }
+    
+    /**
+     * Updates the display of the scorecardController such that the values in the scorecard are presented on the page
+     */
+    private void updateInfoDisplay(){
+        this.currentHoleParLabel.setText("Par: " + Integer.toString(this.scorecard.getCurrentHolePar()));
+        this.previousHoleButton.setText("Prev Hole: " + Integer.toString(this.scorecard.getCurrentHole() - 1));
+        this.nextHoleButton.setText("Next Hole: " + Integer.toString(this.scorecard.getCurrentHole() + 1));
+        this.currentHole.setText("Current Hole: " + Integer.toString(this.scorecard.getCurrentHole()));
+        this.currentScore.setText(Integer.toString(this.scorecard.getCurrentHoleThrows()));
     }
 
-//     @Test
-//    // @DisplayName("Test that the initialized items in controller are not null")
-//     public void testContollerItems(){
-//         assertNotNull(this.controller);
-//     }
-    
-//     @Test
-//     public void testCourseSelection(){
-//         assertEquals(pickCourseMenu.getItems().get(0), lade.getCourseName());
-//         assertEquals(pickCourseMenu.getItems().get(1), dragvoll.getCourseName());
+    /**
+     * Updates the visibility of the submitbutton depending on getCurrentHole() value of scorecard
+     */
+    private void handleBtnVisibilty(){
+        this.previousHoleButton.setVisible(scorecard.getCurrentHole() != 1);
+        this.nextHoleButton.setVisible(scorecard.getCurrentHole() != scorecard.getCourseSize()); 
+        this.submitBtn.setVisible(scorecard.getCurrentHole() == scorecard.getCourseSize());
+    }
 
-       
+    /**
+     * Clicks on the nextHoleButton course.getNumberOfHoles() times, such that the submit button will appear
+     * @param course
+     */
+    private void iterateThroughCourse(Course course){
+        for (int i = 0; i < course.getNumberOfHoles(); i++) {
+            clickOn("#nextHoleButton");
+        }
+    }
 
-//     }
-
-    // @Test 
-    // public void testUnsuccessfulSubmit(){
-    //     assertThrows(IllegalStateException.class, ()->{
-    //         clickOn("#fxmlNameOfPlayer").write("Bob");
-    //         clickOn("#playButton");
-    //         WaitForAsyncUtils.waitForFxEvents();
-    //     });
-    // }
-
-
-   
 }
+
