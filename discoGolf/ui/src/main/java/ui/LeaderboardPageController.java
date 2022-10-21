37442730ba139f71bea0a6bdc7d2525c78f1e239
Ulisman <uisdahl@gmail.com>
2022-10-21 09:35:02 +0200

    Add fields to controller
    Add fields to be able to interact with elements in the fxml
    - Add field for home button
    - Add field for comboBox selection
    - Add field for tableView
    
    Authors: @ulrikyi @jakobop Relates: #88

---
+++ discoGolf/ui/src/main/java/ui/LeaderboardPageController.java
@@ -0,0 +1,23 @@
+package ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableView;
+
+public class LeaderboardPageController {
+  @FXML  
+  public TableView leaderboardTableView;
+  @FXML
+  public Button homeButton;
+  @FXML
+  public ComboBox<String> selectCourseDropdown;
+
+
+  public void initialize(){
+    
+  }
+
+
+
+}
