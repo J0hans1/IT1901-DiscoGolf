0504ba4fafba15b713b8fec22b0d5d27b9711f64
Jakob Opland <jakobop@stud.ntnu.no>
2022-10-21 11:16:54 +0200

    Pull 51-leaderboard and start on LeaderboardPageController
    Need to pull 51-leaderboard branch to get new Leaderboard class
    Start on LeaderboardPageController by adding initialize() and
    displayChosenLeadeboard() methods.
    
    Authors: @jakobop, @ulrikyi Relates: #88

--- discoGolf/ui/src/main/java/ui/LeaderboardPageController.java
+++ discoGolf/ui/src/main/java/ui/LeaderboardPageController.java
@@ -1,8 +1,20 @@
 package ui;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+
+import com.github.javaparser.ast.observer.Observable;
+
+import discoGolf.core.Data;
+import discoGolf.core.Leaderboard;
+import discoGolf.core.Scorecard;
+import discoGolf.json.DiscoGolfPersistence;
+import groovy.util.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
 
@@ -13,17 +25,45 @@ import javafx.scene.control.TableView;
  * @since 2022-09-21
  */
 public class LeaderboardPageController {
+  private Leaderboard leaderboard;
+
   @FXML  
-  public TableView leaderboardTableView;
+  private TableView<Scorecard> leaderboardTableView;
+  @FXML 
+  private TableColumn<Scorecard, Integer> positionColumn, totalScoreColumn; 
+  @FXML 
+  private TableColumn<Scorecard, String> nameColumn;  
   @FXML
   public Button homeButton;
   @FXML
   public ComboBox<String> selectCourseDropdown;
 
-
-  public void initialize(){
-
+  /**
+   * Initialize page by reading data saved and 
+   * creating a new Leaderboard object.
+   * @throws IOException
+   * @throws URISyntaxException
+   */
+  public void initialize() throws IOException, URISyntaxException{
+    DiscoGolfPersistence db = new DiscoGolfPersistence();
+    Data data = new Data();
+    data = db.readData();
+    this.leaderboard = new Leaderboard(data);
+    displayChosenLeadeboard();
   }
+
+  public void displayChosenLeadeboard() {
+    String chosenCourse = selectCourseDropdown.getValue();
+    ArrayList<Scorecard> chosenCourseList = leaderboard.getLeaderboardForCourse(chosenCourse);
+    //positionColumn.setCellValueFactory(new );
+    for (int i = 0; i < chosenCourseList.size(); i++) {
+
+    }
+  }
+
+
+
+
 
 
 
