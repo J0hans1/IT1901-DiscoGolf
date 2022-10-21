6dd12d1760ab224f83657b4596690effb9012be6
Jakob Opland <jakobop@stud.ntnu.no>
2022-10-21 11:52:44 +0200

    Add ScorecardsModel class and work on LeaderboardPageController
    Need to add ScorecardsModel class to be able to display the content in the table view
    Add ScorecardsModel class with field, setters and getters.
    Update LeaderboardPageController to display the correct information
    
    Author: @jakobop, @ulrikyi Relates: #88

--- discoGolf/ui/src/main/java/ui/LeaderboardPageController.java
+++ discoGolf/ui/src/main/java/ui/LeaderboardPageController.java
@@ -4,18 +4,18 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 
-import com.github.javaparser.ast.observer.Observable;
-
 import discoGolf.core.Data;
 import discoGolf.core.Leaderboard;
 import discoGolf.core.Scorecard;
 import discoGolf.json.DiscoGolfPersistence;
-import groovy.util.ObservableList;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 
 /**
@@ -26,13 +26,14 @@ import javafx.scene.control.TableView;
  */
 public class LeaderboardPageController {
   private Leaderboard leaderboard;
+  private ObservableList<ScorecardsModel> scorecardsModel = FXCollections.observableArrayList();
 
   @FXML  
-  private TableView<Scorecard> leaderboardTableView;
+  private TableView<ScorecardsModel> leaderboardTableView;
   @FXML 
-  private TableColumn<Scorecard, Integer> positionColumn, totalScoreColumn; 
+  private TableColumn<ScorecardsModel, Integer> position, totalScore; 
   @FXML 
-  private TableColumn<Scorecard, String> nameColumn;  
+  private TableColumn<ScorecardsModel, String> playerName;  
   @FXML
   public Button homeButton;
   @FXML
@@ -44,20 +45,27 @@ public class LeaderboardPageController {
    * @throws IOException
    * @throws URISyntaxException
    */
-  public void initialize() throws IOException, URISyntaxException{
+  public void initialize() throws IOException, URISyntaxException{    
     DiscoGolfPersistence db = new DiscoGolfPersistence();
     Data data = new Data();
     data = db.readData();
     this.leaderboard = new Leaderboard(data);
     displayChosenLeadeboard();
+
+    position.setCellValueFactory(new PropertyValueFactory<>("StudentId"));
+    playerName.setCellValueFactory(new PropertyValueFactory<>("FirstName"));
+    totalScore.setCellValueFactory(new PropertyValueFactory<>("LastName"));
+    //add your data to the table here.
+    leaderboardTableView.setItems(scorecardsModel);
   }
 
   public void displayChosenLeadeboard() {
     String chosenCourse = selectCourseDropdown.getValue();
     ArrayList<Scorecard> chosenCourseList = leaderboard.getLeaderboardForCourse(chosenCourse);
-    //positionColumn.setCellValueFactory(new );
+ 
     for (int i = 0; i < chosenCourseList.size(); i++) {
-
+      Scorecard scorecard = chosenCourseList.get(i);
+      scorecardsModel.add(new ScorecardsModel(i, scorecard.getPlayerName(), scorecard.getTotalScore()));
     }
   }
 
