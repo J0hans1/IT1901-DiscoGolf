14c2f4989e6157c7fd3abdf6fbd86fc4efffcfac
Markus Aleksander Råkil Johansen <markusrj@stud.ntnu.no>
2022-10-26 17:26:45 +0200

    fetch Database method in DataAccess
    all functionality methods in DataAccess are added
    no methods are tested yet (will be done in next step)
    
    Author: @markusrj Relates: #94

--- discoGolf/ui/src/main/java/ui/DataAccess.java
+++ discoGolf/ui/src/main/java/ui/DataAccess.java
@@ -11,6 +11,10 @@ import discoGolf.json.DiscoGolfPersistence;
 
 // import org.apache.http.HttpException;
 
+/**
+ * Class for accessing the database.
+ * Represents the access layer of the application.
+ */
 public class DataAccess {
   String baseURL = "http://localhost:8080";
   DiscoGolfPersistence persistence = new DiscoGolfPersistence();
@@ -24,21 +28,50 @@ public class DataAccess {
     String scorecardString = persistence.scorecardToJson(scorecard);
 
     try {
+      // Create a request
       HttpRequest request = HttpRequest.newBuilder()
       .uri(new URI(baseURL + "/post2/" + scorecardString))
       .POST(BodyPublishers.ofString(""))
       .build();
 
+      // Send the request and get the response
       final HttpResponse<String> response = HttpClient.newBuilder()
       .build()
       .send(request, HttpResponse.BodyHandlers.ofString());
 
+      // save the response body as a string
       responseBody = response.body();
     } catch (Exception e) {
       e.printStackTrace();
       // throw new HttpException("Error posting scorecard");
     }
-
     return response.body();
+  }
+
+  /**
+   * Forms an HTTP request that will get all scorecards from the database, via the restAPI
+   * @return all scorecards in the database.
+   */
+  public Data fetchDatabase(){
+    Data data = new Data();
+    try {
+      // Create a request
+      HttpRequest request = HttpRequest.newBuilder()
+      .uri(new URI(baseURL + "/get"))
+      .GET()
+      .build();
+
+      // Send the request and get the response
+      final HttpResponse<String> response = HttpClient.newBuilder()
+      .build()
+      .send(request, HttpResponse.BodyHandlers.ofString());
+
+      // Parse the response body into a Data object
+      data = persistence.jsonToData(response.body());
+    } catch (Exception e) {
+      e.printStackTrace();
+      // throw new HttpException("Error fetching database");
+    }
+    return data;
   }
 }
