5435aea723727ff0961de46e4eee6f061c980f9c
Jakob Opland <jakobop@stud.ntnu.no>
2022-10-29 18:04:38 +0200

    Create ScorecardInterface
    Need another ScorecardDAO class for writing from database to Leaderboard
    Create interface for scorecard and scorecardDAO
    
    Authors: @jakobop, Relates: #50

---
+++ discoGolf/core/src/main/java/discoGolf/core/ScorecardDAO.java
@@ -0,0 +1,49 @@
+package discoGolf.core;
+
+public class ScorecardDAO implements ScorecardInterface {
+  private final String playerName;
+  private final Course course;
+  private int bestHole;
+  private int totalScore;
+
+  /**
+     * - constructs a scorecard object for deserializing a json object
+     * 
+     * @param course     is the course the player picked at the main menu
+     * @param playerName is the name of the player
+     * @param totalScore totalscore for the scorecard
+     * @param throwsList all the thrwos+par for each hole
+     */
+    public ScorecardDAO(Course course, String playerName, int totalScore, int bestHole) {
+      this.playerName = playerName;
+      this.course = course;
+      this.totalScore = totalScore;
+      this.bestHole = bestHole;
+  }
+
+  @Override
+  public String getPlayerName() {
+    return playerName;
+  }
+
+  @Override
+  public Course getCourse() {
+    return course;
+  }
+
+  @Override
+  public String getCourseName() {
+    return course.getCourseName();
+  }
+
+  @Override
+  public int getTotalScore() {
+    return totalScore;
+  }
+
+  @Override
+  public int getBestHoleScore() {
+    return bestHole;
+  }
+  
+}
---
+++ discoGolf/core/src/main/java/discoGolf/core/ScorecardInterface.java
@@ -0,0 +1,15 @@
+package discoGolf.core;
+
+public interface ScorecardInterface {
+    
+    public String getPlayerName();
+
+    public Course getCourse();
+
+    public String getCourseName();
+    
+    public int getTotalScore(); 
+
+    public int getBestHoleScore();
+
+}
